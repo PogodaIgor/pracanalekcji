5928a1f80aa5a16c02d155a5542bbe01ea3b5b02
PogodaIgor <igor.pogoda2010@wp.pl>
2025-12-18 08:44:45 +0100

    a

---
+++ app.js
@@ -0,0 +1,12 @@
+function red(){
+        document.getElementById("color1").innerHTML
+        document.getElementById("body").style.backgroundColor = "tomato"
+}
+function green(){
+        document.getElementById("color1").innerHTML
+        document.getElementById("body").style.backgroundColor = "lightgreen"
+}
+function blue(){
+        document.getElementById("color1").innerHTML
+        document.getElementById("body").style.backgroundColor = "lightblue"
+}
